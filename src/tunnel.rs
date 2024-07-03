0990714c6649067ac24cc5ffd4a15f024f8fbe9b
Junior Portilho <junior.portilho2005@gmail.com>
2024-07-03 17:04:00 +0100

    Implemented fn to recieve data size and then recive data

--- src/tunnel.rs
+++ src/tunnel.rs
@@ -27,19 +27,31 @@ pub fn server(ip: String) {
 }
 
 fn handle_conn(mut conn: TcpStream) -> Result<(), std::io::Error> {
-    let mut buffer = [0u8; 1500];
-    match conn.read(&mut buffer) {
-        Ok(0) => {
-            println!("Client disconected");
-        },
-        Ok(n) => {
-            println!("Readed {} bytes from conn", n);
+    let mut buffer = [0u8; 4]; // Buffer to read size of data
+    match conn.read_exact(&mut buffer) {
+        Ok(_) => {
+            println!("Read 4 bytes for data length");
         },
         Err(e) => {
             eprintln!("Error: {}", e);
             return Err(e);
         }
     };
-    println!("Buffer: {}", String::from_utf8(buffer.to_vec()).unwrap());
+    println!("Buffer content: {:?}", buffer);
+
+    let data_size = u32::from_be_bytes(buffer) as usize;
+
+    let mut data_buffer = vec![0u8; data_size];
+    match conn.read_exact(&mut data_buffer) {
+        Ok(_) => {
+            println!("Read {} bytes from conn", data_size);
+        },
+        Err(e) => {
+            println!("Error: {}", e);
+            return Err(e);
+        }
+    }
+
+    println!("Buffer content: {}", String::from_utf8(data_buffer).unwrap());
     Ok(())
 }
